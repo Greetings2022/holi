58905f3828e05d8b3816d42d4bf1a6f7cf050338
Greetings2022 <101033817+Greetings2022@users.noreply.github.com>
2022-03-06 11:17:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+<h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+                 var a=prompt("अपना नाम डाले\n\n👇👇👇👇👇");
+                 document.write(a);
+                </script> </h1> <
+h1 style = "text-transform: uppercase;" > <script type="text/javascript" language="Javascript">
+                    
+                    document.write(a);
+                      </script> < /h1> <
+  h1 style = "text-transform: uppercase;" > <script type="text/javascript" language="Javascript">
+                   
+                    document.write(a);
+                      </script> < /h1> <
+  h1 style = "text-transform: uppercase;" > <script type="text/javascript" language="Javascript">
+                    
+                    document.write(a);
+                      </script> < /h1> <
+  h1 style = "text-transform: uppercase;" > <script type="text/javascript" language="Javascript">
+                   
+                    document.write(a);
+                      </script> < /h1> <
+  h1 style = "text-transform: uppercase;" > <script type="text/javascript" language="Javascript">
+                   
+                    document.write(a);
+                      </script> < /h1> <
+  h1 style = "text-transform: uppercase;" > <script type="text/javascript" language="Javascript">
+                    
+                    document.write(a);
+                      </script> < /h1>
